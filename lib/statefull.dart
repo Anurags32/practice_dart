0e592be7b60ddb0848ea325a21c808325177f967
Anurag Tiwari <anuragtiwaris1172000@gmail.com>
2025-05-26 22:24:25 +0530

    statfull

---
+++ lib/statefull.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class chek extends StatefulWidget {
+  const chek({super.key});
+
+  @override
+  State<chek> createState() => _chekState();
+}
+
+class _chekState extends State<chek> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
