8a05a9af72b5f89f456573dc4e4b49df911a7afd
Anurag Tiwari <anuragtiwaris1172000@gmail.com>
2025-05-26 11:46:20 +0530

    strime

---
+++ lib/may26.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+
+void main() async {
+  stream();
+  await for (int num in countStream()) {
+    print(num);
+  }
+}
+
+void stream() {
+  final controller = StreamController<String>();
+  controller.stream.listen((data) {
+    print("Recive $data");
+  });
+  controller.add("Anurag");
+  controller.add("Tiwari");
+  controller.add("Narendra");
+  controller.close();
+}
+
+Stream<int> countStream() async* {
+  for (int i = 1; i <= 5; i++) {
+    await Future.delayed(Duration(seconds: 1));
+    yield i;
+  }
+}
